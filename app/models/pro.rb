37111893be34f8105d2e9a7f137b472007edb47b
df <duanfeisj@sina.com>
2013-10-09 17:38:50 +0800

    add bootstrap

--- app/models/pro.rb
+++ app/models/pro.rb
@@ -1,2 +1,8 @@
 class Pro < ActiveRecord::Base
+	validates_uniqueness_of :name, :message => "项目名称必须唯一", :on => :create
+	validates_format_of :url, 
+						:with =>  /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix,
+						:message => "请填写合法的项目地址"
+
+
 end
