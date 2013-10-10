4cc4c03e527edeed8ce2375527f547fec3e22f27
df007df <duanfeisj@gmail.com>
2013-10-11 02:46:59 +0800

    add pros map

--- app/models/pro.rb
+++ app/models/pro.rb
@@ -1,4 +1,5 @@
 class Pro < ActiveRecord::Base
+	validates_presence_of :name, :localhost, :url, :branch
 	validates_uniqueness_of :name, :message => "项目名称必须唯一", :on => :create
 	validates_format_of :url, 
 						:with =>  /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Project::Application.routes.draw do
   resources :pros
+  resources :maps
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
