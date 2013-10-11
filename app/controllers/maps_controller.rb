cedf29f80da30bc283b370931df076bf6398f98c
df007df <duanfeisj@gmail.com>
2013-10-11 23:04:44 +0800

    domain to adress to img

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,27 +4,34 @@ class MapsController < ApplicationController
   
 
 	def index
-		
-		pro_list_adress = []
+		@pros = Pro.all
+
+		@pro_list_adress = []
 		#pro_list 
 
-		ip = IPSocket.getaddress('oa.a-y.com.cn')
-		
-		pro_list_adress = getAdressForIp ip
-		
+		@pros.each do |item|
+			
+			if !item.url.empty?
+			 logger.debug item.url
+			 ip = IPSocket.getaddress(item.url)
+			 @pro_list_adress.push getAdressForIp ip
+		  end
+    end
 
-		logger.debug pro_list_adress
-
-
-		
+		@pro_list_adress
 	end	
 
 	def getAdressForIp(ip = '')
 		api = "https://api.weibo.com/2/location/geo/ip_to_geo.json?ip=#{ip}&access_token=2.00TJ877B0wq4Ihccfb424a92Kww4MB"
 		if ip
-
 			response = open(api).read
 			data = JSON::parse(response)
+
+			{:longitude => data['geos'][0]['longitude'], 
+				:latitude => data['geos'][0]['latitude'], 
+				:more => data['geos'][0]['more']
+			}
+
 		end	
 	end	
 
--- app/models/pro.rb
+++ app/models/pro.rb
@@ -1,8 +1,8 @@
 class Pro < ActiveRecord::Base
-	validates_presence_of :name, :localhost, :url, :branch
+	validates_presence_of :name, :localhost, :url, :branch 
 	validates_uniqueness_of :name, :message => "项目名称必须唯一", :on => :create
 	validates_format_of :url, 
-						:with =>  /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix,
+						:with =>  /(^$)|([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix,
 						:message => "请填写合法的项目地址"
 
 
